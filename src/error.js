9a9415a7abef67e68ccbb8e2369b9f17a6a84f81
drsalnikov <drsalnikovev@gmail.com>
2018-10-04 20:24:34 +0300

    Revert "Разделить, не смешивать"

--- src/error.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-module.exports = {
-  name: `error`,
-  description: `Shows error message`,
-  execute(unknownCommand) {
-    console.log(`Unknown command: ${unknownCommand}. Try "--help" to view available commands`);
-  }
-};
