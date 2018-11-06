e7e8e98ebb579abdcc3ee7194ba4bae233d72bc2
Nikolay Solodukhin <Nikolay.Solodukhin@gmail.com>
2018-11-07 01:54:00 +0300

    final correct

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 const express = require(`express`);
 const path = require(`path`);
@@ -7,6 +7,7 @@ const imageStore = require(`./images/store`);
 const postRouter = require(`./posts/route`)(postsStore, imageStore);
 const logger = require(`./logger`);
 const {ERROR_HANDLER, NOT_FOUND_HANDLER} = require(`./error/handlers`);
+const NotImplementedError = require(`./error/not-implemented-error`);
 
 const app = express();
 const {SERVER_PORT = 3000, SERVER_NAME = `localhost`} = process.env;
@@ -21,12 +22,21 @@ const CORS_HANDLER = (req, res, next) => {
   next();
 };
 
-app.use(CORS_HANDLER);
+const NOT_IMPLEMENTED = (req, res, next) => {
+  const allowedMethods = [`GET`, `POST`];
+  if (allowedMethods.includes(req.method)) {
+    next();
+  } else {
+    throw new NotImplementedError(`Method not implemented`);
+  }
+};
 
+app.use(CORS_HANDLER);
 app.use(express.static(staticPath));
 app.use(`/api/posts`, postRouter);
 app.use(NOT_FOUND_HANDLER);
 app.use(ERROR_HANDLER);
+app.all(NOT_IMPLEMENTED);
 
 const start = (port) => {
   app.listen((port = SERVER_PORT), SERVER_NAME, () =>
